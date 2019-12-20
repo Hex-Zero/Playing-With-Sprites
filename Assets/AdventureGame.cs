fd06e2747b78d32d4c10bf0f3d869a6554a9d98f
Hex-Zero <hex0@live.com>
2019-12-20 20:43:29 +0000

    init

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text TextComponet;
+    void Start()
+    {
+        TextComponet.text = "Ace Alrich";
+    }
+
+    void Update()
+    {
+        
+    }
+}
