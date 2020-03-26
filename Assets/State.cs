85cfc92e07883dcc665981fad5168ba584a1f9ac
Hex-Zero <hex0@live.com>
2020-03-26 19:39:37 +0000

    Attach States

--- Assets/State.cs
+++ Assets/State.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [TextArea(10, 14)] [SerializeField] string storyText;
-
+    [SerializeField] State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
     }
+    public State[] GetNextState()
+    {
+        return nextStates;
+    }
+
 }
